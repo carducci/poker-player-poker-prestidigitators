085c44bb622c2b8fc76f7e384a808aba330caad7
Michael Carducci <michael@trulymagic.com>
2024-05-28 17:44:18 +0200

    folding postflop

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -51,6 +51,9 @@ public class Player {
         if (random.nextInt(4) == 0) {
           return betRequest.pot();
         }
+        if(doWeHaveToCallAnAllIn(betRequest)){
+          return 0;
+        }
         return betRequest.pot() / 4;
       }
 
