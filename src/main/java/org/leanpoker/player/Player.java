cf89c7eb90cc298037f75baa09129449058494a0
Lars Eckart <lars.eckart@hey.com>
2024-05-28 11:22:28 +0200

    getting tests in place

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,12 +1,18 @@
 package org.leanpoker.player;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
 
 public class Player {
+
+    private static final Logger log = getLogger(Player.class);
 
     static final String VERSION = "1.1";
 
     public static int betRequest(JsonNode request) {
+        log.info(request);
         return 500;
     }
 
---
+++ src/test/java/org/leanpoker/player/APlayer.java
@@ -0,0 +1,11 @@
+package org.leanpoker.player;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class APlayer {
+
+}
